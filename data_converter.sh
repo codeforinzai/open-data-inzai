db9809761114fa2670fd304e5e0e52131ae2b623
jollyjoester <jollyjoester@gmail.com>
2021-06-01 19:04:57 +0900

    Convert from csv to json

--- data_converter.sh
+++ data_converter.sh
@@ -1,5 +1,13 @@
-for file in `\find ./original -type f | awk -F/ '{print $NF}'`; do
-  iconv -f sjis -t utf-8 ./original/$file > ./csv/$file
-  nkf -Lu --overwrite ./csv/$file
-done
+# Convert original csv
+for file in `find ./original/*.csv -type f -exec basename -s .csv {} \;`; do
+  # Convert character code from SJIS to UTF-8
+  iconv -f sjis -t utf-8 ./original/$file.csv > ./csv/$file.csv
 
+  # Convert line feed code to LF
+  nkf -Lu --overwrite ./csv/$file.csv
+
+  # Convert from csv to json
+  csv2json -d ./csv/$file.csv ./json/tmp_$file.json
+  cat ./json/tmp_$file.json | jq . > ./json/$file.json
+  rm -rf ./json/tmp_*.json
+done
